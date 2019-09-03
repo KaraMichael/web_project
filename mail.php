05b204979d7d00f9f9f53919c1d20d22f2304e3d
Michael Kara <michael.kara@webrepublic.ch>
2019-09-03 15:01:27 +0200

    Finish: add pictures, text, change some little things

--- mail.php
+++ mail.php
@@ -1,35 +1,28 @@
 <?php
 
-  if($_POST["Name"] != "" && $_POST["Email"] != "" && $_POST["nachricht"] != "")
+  if($_POST["NameF"] != "" && $_POST["EmailF"] != "" && $_POST["MessageF"] != "")
   {
 
-    $name = $_POST["Name"];
-    $email = $_POST["email"];
-    $nachricht = $_POST["nachricht"];
-    $anrede = $_POST["anrede"];
-    $vorname = $_POST["vorname"];
-    $organisation = $_POST["organisation"];
-    $telefon = $_POST["tel"];
-    $betreff = $_POST["betreff"];
+    $name = $_POST["NameF"];
+    $email = $_POST["EmailF"];
+    $nachricht = $_POST["MessageF"];
+    $betreff = $_POST["SubjectF"];
 
     $headers   = array();
     $headers[] = "MIME-Version: 1.0";
     $headers[] = "Content-type: text/plain; charset=utf-8";
-    $headers[] = "From: Kontaktformular Website <kontakt@coatchecker.ch>";
+    $headers[] = "From: Kontaktformular Website <contact@info.ch>";
     //$headers[] = "To: Info <info@coatchecker.ch>, Alexander Bleuler <a.bleuler@coatchecker.ch>, Techsupport <techsupport@coatchecker.ch>";
-	$headers[] = "To: Info <info@coatchecker.ch>";
+	  $headers[] = "To: Michael Kara <michael.kara@hotmail.com>";
     $headers[] = "Reply-To: ".$email;
     $headers[] = "X-Mailer: PHP/".phpversion();
 
     $precontent = "
 ------------------------------------ Kontaktinfo ---------------------------------------
 
-Anrede: ".$anrede."
-Vorname: ".$vorname."
 Name: ".$name."
 E-Mail: ".$email."
-Telefon: ".$telefon."
-Organisation: ".$organisation."
+Betreff: ".$betreff."
 
 -------------------------------------- Nachricht ---------------------------------------
 
